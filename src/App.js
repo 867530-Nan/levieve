55377f922ec697985a541ce3948bf8817156a343
Matthew Peterson <mbpeterson19@gmail.com>
2019-09-16 11:45:32 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Logo from './mattLogoEdits.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p className='pHeader'>Coming Soon</p>
+      <div className='divider' />
+      <img src={Logo} className='logo' />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
